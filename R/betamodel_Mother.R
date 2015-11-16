cf2915aadbc5ee5b8c6dd5d1e2497266003d6fe7
Ian Wilson <wilson.ianj@gmail.com>
2015-11-16 16:15:22 +0000

    got first set of files added

---
+++ R/betamodel_Mother.R
@@ -0,0 +1,17 @@
+basedir <- paste(normalizePath("~"),"IHG/projects/bottle",sep="/")
+
+bvec <- c(0.4,0.7,0.9)
+args = commandArgs(TRUE)
+b=bvec[as.integer(args[1])]
+
+source(paste(basedir,"R/prepareSimData.R",sep="/"))
+library(coda)
+library(rjags)
+
+
+modelfile <- paste(basedir,"JAGSmodels/mutbeta.jag",sep="/")
+model.jags <- jags.model(modelfile,data=simMotherData,n.chain=2)
+update(model.jags,n.iter=10000)                                    # burn in 
+betaresMother <- coda.samples(model.jags,c("sigmam","b"),n.iter=200000,thin=20)
+save(betaresMother,file=paste("betaresMother",b,".rda",sep=""))
+q("no")
---
+++ R/prepareSimData.R
@@ -0,0 +1,45 @@
+getMotherDaughters <- function(nfamilies,b,d,betapars,noffspring=3) {
+  het_mother <- rbeta(nfamilies,betapars[1],betapars[2])  
+  alpha <- het_mother*b/(1-b)
+  beta <- (1-het_mother)*b/(1-b)
+  daughters <- matrix(rbeta(noffspring*nfamilies,alpha,beta),ncol=noffspring)
+  pselected <- (daughters[,1]-d[1])/(d[2]-d[1])
+  proband <- runif(nfamilies) < pselected
+  motherproband <- runif(nfamilies) < (het_mother-d[1])/(d[2]-d[1])
+  list(het_mother=het_mother,het_daughters=daughters,proband=proband,motherproband = motherproband)
+}
+
+
+
+aa <- getMotherDaughters(40000,b=b,d=c(0.3,0.7),betapars=c(1,4))
+
+ss <- min(sum(aa$motherproband==TRUE),sum(aa$proband==TRUE&aa$motherproband==FALSE),100)
+if (ss<100) {
+  cat("problem with first data set\n")
+  cat("we only have sample size of",ss)
+}  else {
+  cat("We have simulated", min(sum(aa$motherproband==TRUE),sum(aa$proband==TRUE)),"potential samples, taking the first 100\n")
+}
+## convert into three data sets each with 100 individuals
+simMotherData <- list(parent = aa$het_mother[aa$motherproband==TRUE][1:ss]
+                      ,off = aa$het_daughters[aa$motherproband==TRUE,][1:ss,]
+                      ,N = rep(3,ss)
+                      ,NFam=ss
+                      ,mutation=rep(1,ss)
+                      ,Nmuts=1)
+
+simProbandData <- list(parent = aa$het_mother[aa$proband==TRUE&aa$motherproband==FALSE][1:ss]
+                       ,off = aa$het_daughters[aa$proband==TRUE&aa$motherproband==FALSE,1,drop=FALSE][1:ss,,drop=FALSE]
+                      ,N = rep(1,ss)
+                       ,NFam=ss
+                       ,mutation=rep(1,ss)
+                       ,Nmuts=1)
+  
+simSiblingData   <- list(parent = aa$het_mother[aa$proband==TRUE&aa$motherproband==FALSE][1:ss]
+                         ,off = aa$het_daughters[aa$proband==TRUE&aa$motherproband==FALSE,2:3][1:ss,]
+                         ,N = rep(2,ss)
+                         ,NFam=ss
+                         ,mutation=rep(1,ss)
+                         ,Nmuts=1)
+
+rm(aa)
